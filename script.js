64b9267cc6506041398266cc0d98b527119afb3a
jcosens@umich.edu <jcosens@umich.edu>
2020-04-04 18:08:01 -0400

    Added pics

--- script.js
+++ script.js
@@ -1,7 +1,39 @@
 var resultView = new Vue({
     el: '#app',
     data: {
-      locations: ['./img/bighouse.jpg', './img/lawquad.jpg'],
+      locations: 
+      [
+        {
+          name: 'big house',
+          img: './img/bighouse.jpg', 
+          selected: false
+        },
+        {
+          name: 'law quad',
+          img: './img/lawquad.jpg', 
+          selected: false
+        },
+        {
+          name: 'michigan theater',
+          img: './img/michigantheater.jpg', 
+          selected: false
+        },
+        {
+          name: 'skeeps',
+          img: './img/skeeps.jpg', 
+          selected: false
+        },
+        {
+          name: 'ugli',
+          img: './img/ugli.jpg', 
+          selected: false
+        },
+        {
+          name: 'blue bus',
+          img: './img/bluebus.jpg', 
+          selected: false
+        },
+      ],
     },
     methods: {
 
