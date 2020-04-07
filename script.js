e83e13ccb2212b2105badab3487c921e16c0a583
jcosens@umich.edu <jcosens@umich.edu>
2020-04-06 21:25:58 -0400

    If ice block is hit, it disappears, but only if you hit the first block

--- script.js
+++ script.js
@@ -49,6 +49,7 @@ var resultView = new Vue({
       leftpos_shooter: 400,
       snowballIndex: 0,
       snowballLimit: 32,
+      points: 0
     },
     methods: {
       selectLocation(event, location) {
@@ -181,8 +182,49 @@ var resultView = new Vue({
 
   })
 
+  function graduallyFadeAndRemoveElement(elementObj){
+    // Fade to 0 opacity over 2 seconds
+    elementObj.fadeTo(500, 0, function(){
+      $(this).remove();
+    });
+  }
+
+  function isOrWillCollide(o1, o2, o1_xChange, o1_yChange){
+    const o1D = { 'left': o1.offset().left + o1_xChange,
+          'right': o1.offset().left + o1.width() + o1_xChange,
+          'top': o1.offset().top + o1_yChange,
+          'bottom': o1.offset().top + o1.height() + o1_yChange
+    };
+    const o2D = { 'left': o2.offset().left,
+          'right': o2.offset().left + o2.width(),
+          'top': o2.offset().top,
+          'bottom': o2.offset().top + o2.height()
+    };
+    // Adapted from https://developer.mozilla.org/en-US/docs/Games/Techniques/2D_collision_detection
+    if (o1D.left < o2D.right &&
+      o1D.right > o2D.left &&
+      o1D.top < o2D.bottom &&
+      o1D.bottom > o2D.top) {
+       // collision detected!
+       return true;
+    }
+    return false;
+  }
+
   window.addEventListener('keydown', function(e) {
     resultView.handleGlobalKeyDown(e);
+    this.setInterval(function(){
+      $(".snowball").each(function( element ) {
+        snowballObject = $(this)
+        $("#iceBlocks").each(function( element ) {
+          if(isOrWillCollide(snowballObject, $(this),0 ,0)){
+            $(this).attr("src", "./img/hitIceBlock.png");
+            $(this).css("width", "100px");
+            $(this).attr("id", "hitIceBlock");
+          }
+        })
+      });
+    }, 10)
   });
   
 
