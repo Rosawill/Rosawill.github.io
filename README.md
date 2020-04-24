393c8229b866ef1a57c7b562bdce690a54f839b6
Rosawill <rosawill@umich.edu>
2020-04-24 15:03:27 -0400

    we're really fixin it up here

--- README.md
+++
@@ -1,1 +0,0 @@
-# Rosawill.github.io
--- script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready( function() {
-  createVueObjects();
-  /*
-  $(window).keydown(function(e) {
-    resultView.handleGlobalKeyDown(e);
-  });
-  */
-})
